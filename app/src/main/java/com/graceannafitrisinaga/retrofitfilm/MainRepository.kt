78f8e7278fddb2ba9f6b41a647cb2cef2340fd0c
Grace Anna Fitri Sinaga <grace.anna@students.utdi.ac.id>
2022-11-23 12:01:44 +0700

    Update MainRepository.kt

--- app/src/main/java/com/graceannafitrisinaga/retrofitfilm/MainRepository.kt
+++ app/src/main/java/com/graceannafitrisinaga/retrofitfilm/MainRepository.kt
@@ -1,6 +1,9 @@
+//nama paket
 package com.graceannafitrisinaga.retrofitfilm
 
+//class untuk menangani data dari API dengan menggunakan primary contructor dimana parameternya menggunakan hak akses private pada properti retrofitService yang memanggil kelas RetrofitService
 class MainRepository constructor(private val retrofitService: RetrofitService) {
 
+    //meneruskan instance retrofitService untuk melakukan panggilan jaringan
     fun getAllMovies() = retrofitService.getAllMovies()
 }
