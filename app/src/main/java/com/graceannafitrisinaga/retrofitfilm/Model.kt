0f973e28c4d3a038c62500c3e64289ca6239479d
Grace Anna Fitri Sinaga <grace.anna@students.utdi.ac.id>
2022-11-23 11:22:25 +0700

    Update Model.kt

--- app/src/main/java/com/graceannafitrisinaga/retrofitfilm/Model.kt
+++ app/src/main/java/com/graceannafitrisinaga/retrofitfilm/Model.kt
@@ -1,5 +1,5 @@
 //nama paket
 package com.graceannafitrisinaga.retrofitfilm
 
-//class Movie yang digunakan untuk menampung data variabel name, imageUrl, category, desc yang ditandai dengan tipe data nya masing-masing menggunakan val
+//class Movie yang digunakan untuk menampung data properti variabel name, imageUrl, category, desc yang ditandai dengan tipe data nya masing-masing menggunakan val
 data class Movie(val name: String, val imageUrl: String, val category: String, val desc: String)
